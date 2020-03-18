d6939de9016fb77994223c1a61865bd013d01e7b
Warakorn1288 <62011288@kmitl.ac.th>
2020-03-18 12:17:18 +0700

    week 8.1

---
+++ week_8.1.ino
@@ -0,0 +1,50 @@
+
+float brightness = 0;
+String buff;
+int state;
+int pin;
+void setup(){
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  if (Serial.available() > 0)
+  {
+    char inByte = Serial.read(); //L1B80
+   
+    if(inByte == 'L')
+    {
+      state = 0;
+    }
+ 
+ 
+  if(('0'<= inByte) && (inByte <= '9') && (state == 0))
+    {
+       if(inByte == '1'){
+         pin = 5;
+       }
+       if(inByte == '2'){
+         pin = 6;
+       }
+    }
+         
+    if(inByte == 'B'){
+      buff = ' ';
+      state = 1;
+    }
+             
+    if(('0'<= inByte) && (inByte <= '9') && (state == 1))
+     {
+         buff += inByte;
+     }
+         
+       if(inByte == 'A')
+       {
+         brightness = buff.toInt();
+         Serial.println(brightness);
+         analogWrite(pin,brightness*2.54);
+       }
+     }
+ }
